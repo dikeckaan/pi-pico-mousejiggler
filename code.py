6321ee66621ace4937d600ab17711e81b12287f5
KAAN DIKEC <21171681+dikeckaan@users.noreply.github.com>
2022-06-22 12:03:34 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,9 +12,8 @@ led = digitalio.DigitalInOut(board.GP25) #led defined
 led.direction = digitalio.Direction.OUTPUT #led value
 
 
-led.value = False
+led.value = False #Do you want activate led?
 time.sleep(0) #You can set bigger than 0 if you want wait before cycle
-
 depth=5 #Movement area
 betw=120 #Time between movements
 wait=120 #Wait time for restart Cycle
@@ -22,15 +21,15 @@ wait=120 #Wait time for restart Cycle
 
 while True:
 	print("Controller is", microcontroller.cpu.temperature, "°C") #Celcius value of device cpu
-	led.value = True
+	led.value = not led.value
 	mouse.move(x=depth)
 	#mouse.move(y=depth)
-	led.value = False
+	led.value = not led.value
 	print(betw ,"sec wait")
 	time.sleep(betw)
-	led.value = True
+	led.value = not led.value
 	mouse.move(x=-depth)
 	#mouse.move(y=-depth)
-	led.value = False
+	led.value = not led.value
 	print("time is up, cycle restarting after", wait, "second")
 	time.sleep(wait)
